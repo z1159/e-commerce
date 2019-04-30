7a9743f61787b28205038d61874144db50e6049f
bj <bj991006@163.com>
2019-04-30 14:20:03 +0800

    消费者发表评论controller层(路径单纯打开发表评论页面) bj

---
+++ shop-feign/src/main/java/com/mr/commentList/controller/CommentListController.java
@@ -0,0 +1,23 @@
+package com.mr.commentList.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("CommentListController")
+public class CommentListController {
+
+    /**
+     * 进入发表品论页面url:localhost:8765/CommentListController/goToCommentList
+     * @return
+     */
+    @GetMapping("goToCommentList")
+    public ModelAndView goToCommentList(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("bj/commentList");
+        return mv;
+    }
+
+}
