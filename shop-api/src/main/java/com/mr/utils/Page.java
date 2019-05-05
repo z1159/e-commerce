6adbd10cc5dc30f59a4892277e03b606715c29e3
zhang <wanfengwar@163.com>
2019-05-05 15:59:46 +0800

    Page 工具类修改

--- shop-api/src/main/java/com/mr/utils/Page.java
+++ shop-api/src/main/java/com/mr/utils/Page.java
@@ -1,5 +1,8 @@
 package com.mr.utils;
 
+import lombok.Data;
+
+@Data
 public class Page {
 	//下一页(页数) 
 	private Integer page = 1;
@@ -7,29 +10,11 @@ public class Page {
 	//每页条数
 	private Integer limit=10;
 
+	private String sort;
+
+	private String order = "desc";
 
 
 
 
-	public Integer getPage() {
-		return page;
-	}
-
-	public void setPage(Integer page) {
-		this.page = page;
-	}
-
-
-
-
-
-
-
-	public Integer getLimit() {
-		return limit;
-	}
-
-	public void setLimit(Integer limit) {
-		this.limit = limit;
-	}
 }
