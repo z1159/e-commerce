181d9d29a1740dabacc6280248f97347cc9117c0
zhang <wanfengwar@163.com>
2019-05-05 11:25:56 +0800

    client 启动文件

--- shop-client/src/main/java/com/mr/ShopClientApplication.java
+++ shop-client/src/main/java/com/mr/ShopClientApplication.java
@@ -1,9 +1,11 @@
 package com.mr;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableSwagger2Doc
 @SpringBootApplication
 @EnableEurekaClient
 public class ShopClientApplication {
