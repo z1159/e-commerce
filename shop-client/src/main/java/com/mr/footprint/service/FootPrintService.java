9e4f7e72b1fa8666bc9c4f69771f310f4048303c
xueheng <1770948628@qq.com>
2019-05-07 14:51:42 +0800

    足迹提供者界面

--- shop-client/src/main/java/com/mr/footprint/service/FootPrintService.java
+++ shop-client/src/main/java/com/mr/footprint/service/FootPrintService.java
@@ -1,7 +1,13 @@
 package com.mr.footprint.service;
 
+import com.mr.shop.Commodity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by asus on 2019/5/6.
  */
 public interface FootPrintService {
+    void zujiMogo(HttpServletResponse response, HttpServletRequest request, Commodity commodity);
 }
