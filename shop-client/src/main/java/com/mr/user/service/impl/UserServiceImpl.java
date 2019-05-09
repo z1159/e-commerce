cddbca63022975930eefcde457c67e24f11aa582
zry <3581308346@qq.com>
2019-05-09 14:42:46 +0800

    提交User模块

--- shop-client/src/main/java/com/mr/user/service/impl/UserServiceImpl.java
+++ shop-client/src/main/java/com/mr/user/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Created by Administrator on 2019/4/30.
@@ -59,10 +60,10 @@ public class UserServiceImpl implements UserService{
 
         User dbUser = userMapper.loginUser(user.getUserName());
 
-        //判断状态
-        if(dbUser.getUserState()==2){
-            return ResultVo.error(500,"对不起,您的账号不可用");
-        }
+//        //判断状态
+//        if(dbUser.getUserState()==2){
+//            return ResultVo.error(500,"对不起,您的账号不可用");
+//        }
 
         //判断密码
         if(!dbUser.getUserPass().equals(dbUser.getUserPass())){
@@ -72,22 +73,36 @@ public class UserServiceImpl implements UserService{
         return ResultVo.success("登录成功");
     }
 
+    //校验账号唯一
+    @Override
+    public ResultVo userByName(String userName) {
+//        Boolean bool = Pattern.matches("1\\d{10}",userName);
+//        if(!bool){
+//            return ResultVo.error(500,"账号格式有误.");
+//        }
+        User user = userMapper.loginUser(userName);
+         if (user !=null){
+             return ResultVo.error(500,"账号已存在，请跟换");
+         }
+         return ResultVo.success("校验成功，账号可以注册，请完成注册");
+    }
 
-    //注册
+
+    //前台注册
     @Override
     public ResultVo registerUser(User user) {
-        //判断用户名
-       User dbUser = userMapper.loginUser(user.getUserName());
+        if(null==user){
+            return ResultVo.error(500,"请输入用户名或密码");
+        }
 
-       if(null!= dbUser){
+        //校验用户名
+        User dbUser = userMapper.loginUser(user.getUserName());
+
+        //校验用户唯一
+       if(null != dbUser){
            return ResultVo.error(500,"该用户已存在,请跟换");
        }
-       //校验手机号
-        if(null !=dbUser){
-           if (!user.getUserPhone().equals(user.getUserPhone())){
-               return ResultVo.error(500,"手机号已存在,请跟换");
-           }
-        }
+
        //添加用户
         userMapper.saveUser(user);
         return ResultVo.success("恭喜用户["+user.getUserName()+"]注册成功");
